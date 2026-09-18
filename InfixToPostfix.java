44cd399f62e5104066f8ba420da1ec3edce2306c
SteveGoodman18 <sgoodman@glenbrook225.org>
2026-09-18 14:10:29 -0500

    Create InfixToPostfix utility class
    This class provides methods to convert infix expressions to postfix notation and evaluate operator precedence.

---
+++ InfixToPostfix.java
@@ -0,0 +1,35 @@
+/**
+ * Utility class for converting infix expressions to postfix notation.
+ * 
+ * @author [Student Name]
+ * @version [Date]
+ */
+public class InfixToPostfix {
+
+    /**
+     * Converts a valid infix expression into a space-delimited postfix expression.
+     * 
+     * @param infix The input infix expression string.
+     * @return The space-delimited postfix expression string.
+     */
+    public static String convertToPostfix(String infix) {
+        // TODO: Instantiate your custom stack implementation
+        // StackInterface<Character> stack = new LinkedStack<>();
+        
+        // TODO: Implement infix-to-postfix conversion algorithm
+
+        return "";
+    }
+
+    /**
+     * Returns the operator precedence level.
+     * 
+     * @param symbol Operator character ('+', '-', '*', '/', '%', '^', '(')
+     * @return Integer rank: 3 for '^', 2 for '*', '/', '%', 1 for '+', '-', 0 for '('
+     */
+    public static int precedence(char symbol) {
+        // TODO: Implement precedence evaluation
+        
+        return 0;
+    }
+}
