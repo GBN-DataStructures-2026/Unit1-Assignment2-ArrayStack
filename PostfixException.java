201e8d6d1836245d0c52a7d44fdd5401f47315e8
SteveGoodman18 <sgoodman@glenbrook225.org>
2026-09-23 14:34:32 -0500

    Add PostfixException class for error handling

---
+++ PostfixException.java
@@ -0,0 +1,18 @@
+/**
+ * This PostfixException object . . .
+ * 
+ * @author Mr. G.
+ * @version October 10, 2012
+ */
+public class PostfixException extends Exception
+{
+    public PostfixException()
+    {
+        super();
+    }
+    
+    public PostfixException(String message)
+    {
+        super(message);
+    }
+}
